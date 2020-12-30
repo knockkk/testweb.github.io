e44149a0ef668a6230eb6fc5c6c0a9886ebaa2e2
knockkk <pengyu.oka@bytedance.com>
2020-12-30 12:19:11 +0800

    deploy:blog

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+mv docs/note tmp/
 rm -rf docs
 npm run build
 mv ./blog/.vuepress/dist ./docs #部署路径
+mv tmp docs/note
+
 git add .
-git commit -m "deploy"
+git commit -m "deploy:blog"
 git push origin master
 
